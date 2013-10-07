8729068e7d9ac2ba46ae839670e095e2df0b0394
Gena <genagenson@gmail.com>
2013-10-07 17:31:28 +0400

    first version with four static images

--- app/js/app.js
+++ app/js/app.js
@@ -2,9 +2,18 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
+// angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives', 'myApp.controllers']).
+//   config(['$routeProvider', function($routeProvider) {
+//     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
+//     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+//     $routeProvider.otherwise({redirectTo: '/view1'});
+//   }]);
+
+var myApp = angular.module('chooseCar', [])
+	.config(['$routeProvider', function( $routeProvider ) {
+		$routeProvider.when('/game', { templateUrl: 'partials/startGame.html', controller: 'StartGameCtrl'});
+		$routeProvider.when('/records', { templateUrl: 'partials/records.html', controller: 'RecordsCtrl'});
+		$routeProvider.when('/about', { templateUrl: 'partials/about.html', controller: 'AboutCtrl'});
+		$routeProvider.when('/main', { templateUrl: 'partials/main.html', controller: 'MainCtrl'});
+		$routeProvider.otherwise({ redirectTo: '/main'});
+	}]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,35 @@
 'use strict';
 
 /* Controllers */
+function StartGameCtrl( $scope, $http ){
+	$http.get('cars/cars.json').success( function( data ) {
+		var randomNumber = Math.floor( Math.random() * 4 ); //from 0 to 3
+		$scope.randomCarName = data[ randomNumber ].name;
+		$scope.cars = data;
+	});
 
-angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+	$scope.checkChoice = function( car ) {
+		if ( car.name == $scope.randomCarName ) {
+			alert('Rigth!')
+		} else {
+			alert('wrong!');
+		}
+	}
+}
 
-  }])
-  .controller('MyCtrl2', [function() {
+function AboutCtrl( $scope ) {
 
-  }]);
+}
+
+function RecordsCtrl( ) {
+
+}
+
+function MainCtrl( ) {
+
+}
+
+myApp.controller('StartGameCtrl', [ '$scope', '$http', StartGameCtrl])
+	.controller('RecordsCtrl', [ RecordsCtrl])
+	.controller('AboutCtrl', [ AboutCtrl])
+	.controller('MainCtrl', [ MainCtrl]);
