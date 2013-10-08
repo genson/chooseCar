b7ef25a9314980858cea3e4a0025a1c8216c62da
Gena <genagenson@gmail.com>
2013-10-08 11:47:46 +0400

    put functions of game in scope of controller

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,52 +9,53 @@ function StartGameCtrl( $scope, $http ){
 	$http.get('cars/cars.json').success( function( data ) {
 		//all cars from json
 		$scope.carsData = data;
-		updateGame( $scope );
+		$scope.updateGame();
 	});
 
 	$scope.checkChoice = function( car ) {
 		if ( car.name == $scope.randomCarName ) {
 			alert('Rigth!');
-			updateGame( $scope, 50 );
+			$scope.updateGame( 50 );
 
 		} else {
 			alert('wrong!');
-			updateGame( $scope );
+			$scope.updateGame();
 		}
 	}
-}
-
-function updateGame ( $scope, /* optional */ score, numberOfCars ) {
-	var tempArray = [],
-		score = score || 0,
-		numberOfCars = numberOfCars || 4,
-		randomNumber = Math.floor( Math.random() * numberOfCars );
-
-	$scope.userScore += score;
-	$scope.bestScore = ( $scope.userScore > $scope.bestScore ) ? $scope.userScore : $scope.bestScore;
-
-	if ( $scope.gameEnd ) {
-		endGame( $scope );
-		return;
+	
+	$scope.updateGame = function( /* optional */ score, /* optional */ numberOfCars ) {
+		var tempArray = [],
+			score = score || 0,
+			numberOfCars = numberOfCars || 4,
+			randomNumber = Math.floor( Math.random() * numberOfCars );
+	
+		$scope.userScore += score;
+		$scope.bestScore = ( $scope.userScore > $scope.bestScore ) ? $scope.userScore : $scope.bestScore;
+	
+		if ( $scope.gameEnd ) {
+			$scope.endGame();
+			return;
+		}
+	
+		//playing until we have a cars
+		if ( $scope.carsData.length <= numberOfCars ) {
+			randomNumber = Math.floor( Math.random() * $scope.carsData.length );
+			$scope.gameEnd = true;
+		}
+	
+		for ( var i = 0; i < numberOfCars; i++ ) {
+			tempArray.push( $scope.carsData.pop() );
+		}
+	
+		$scope.randomCarName = tempArray[ randomNumber ].name;
+		$scope.cars = tempArray;
 	}
 
-	//playing until we have a cars
-	if ( $scope.carsData.length <= numberOfCars ) {
-		randomNumber = Math.floor( Math.random() * $scope.carsData.length );
-		$scope.gameEnd = true;
+	$scope.endGame = function(){
+		localStorage.setItem( 'bestScore', $scope.bestScore );
+		$scope.cars = [];
 	}
 
-	for ( var i = 0; i < numberOfCars; i++ ) {
-		tempArray.push( $scope.carsData.pop() );
-	}
-
-	$scope.randomCarName = tempArray[ randomNumber ].name;
-	$scope.cars = tempArray;
-}
-
-function endGame( $scope ){
-	localStorage.setItem( 'bestScore', $scope.bestScore );
-	$scope.cars = [];
 }
 
 function AboutCtrl( $scope ) {
