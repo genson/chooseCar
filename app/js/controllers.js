091de7d1a34f77c2514ef16aae8aa8c760455b3d
Gena <genagenson@gmail.com>
2013-10-11 01:14:59 +0400

    adding progressbar, timer and attemps

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,10 @@
 'use strict';
 
 /* Controllers */
-function StartGameCtrl( $scope, $http ){
+function StartGameCtrl( $scope, $http, $location ){
 	$scope.userScore = 0;
 	$scope.bestScore = localStorage.getItem("bestScore") || 0;
+	$scope.attemps = 3;
 	$scope.gameEnd = false;
 
 	$http.get('cars/cars.json').success( function( data ) {
@@ -12,13 +13,56 @@ function StartGameCtrl( $scope, $http ){
 		$scope.updateGame();
 	});
 
+	$scope.progressbarLength = function( timeForChoose ){
+		var timeForChoose = timeForChoose || 10000;
+
+		var timer = setInterval(function(){
+        	$scope.$apply(function() {
+        		var value = $scope.progressbar.width,
+            		type;
+            	
+            	$scope.progressbar.width = value + 0.5;
+            	
+            	if (value < 45) {
+    			  type = 'success';
+    			} else if (value < 70) {
+    			  type = 'info';
+    			} else if (value < 95) {
+    			  type = 'warning';
+    			} else {
+    			  type = 'danger';
+    			}
+
+    			$scope.progressbar.status = type;
+        	});
+        	if (  $scope.progressbar.width >= 100 ) {
+        		clearInterval(timer);
+
+        		$scope.attemps--;
+        		if ( $scope.attemps ) {
+					alert('Fail! ' + $scope.attemps + ' attemps left!');
+        			$scope.updateGame();
+        		} else {
+        			alert('You lose! Your score:' + $scope.userScore);
+        			window.location.href = '#/main';
+        		}
+        	} 
+   		}, timeForChoose / 200);
+	}
+	
+	$scope.progressbar = {
+		'status' : '',
+		'width' : 0
+	}
+
 	$scope.checkChoice = function( carName ) {
 		if ( carName == $scope.randomCarName ) {
 			alert('Rigth!');
+			
 			$scope.updateGame( 50 );
 
 		} else {
-			alert('wrong!');
+			alert('Fail!');
 			$scope.updateGame();
 		}
 	}
@@ -31,7 +75,9 @@ function StartGameCtrl( $scope, $http ){
 	
 		$scope.userScore += score;
 		$scope.bestScore = ( $scope.userScore > $scope.bestScore ) ? $scope.userScore : $scope.bestScore;
-	
+		$scope.progressbar.width = 0;
+		$scope.progressbarLength(1000);
+
 		if ( $scope.gameEnd ) {
 			$scope.endGame();
 			return;
@@ -53,13 +99,17 @@ function StartGameCtrl( $scope, $http ){
 	
 		$scope.randomCarName = tempArray[ randomNumber ].name;
 		$scope.cars = tempArray;
+
+
 	}
 
 	$scope.endGame = function(){
 		localStorage.setItem( 'bestScore', $scope.bestScore );
 		$scope.cars = [];
+		$scope.gameResult = "You lose :(";
 	}
 
+	
 }
 
 function AboutCtrl( $scope ) {
