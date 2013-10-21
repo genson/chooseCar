d84f5fe655ed9e672483b59456e7abfb5871be33
Gena <genagenson@gmail.com>
2013-10-21 16:27:32 +0400

    add task for two-step publication on gh-pages with min and concat sources

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,7 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'), 
- 
+        pkg: grunt.file.readJSON('package.json'),
+
         jshint: {     // описываем как будет проверять наш код - jsHint
           options: {
             curly: true,
@@ -20,58 +20,57 @@ module.exports = function (grunt) {
               console: true
             }
           },
-          '<%= pkg.name %>': {  
-            src: [ 'app/js/**/*.js' ]  
+          '<%= pkg.name %>': {
+            src: [ 'app/js/**/*.js' ]
           }
         },
- 
-        concat: { 
+
+        concat: {
             dist: {
-                src: ['app/js/*.js'],  
-                dest: 'build/build.js'
+                src: ['app/js/*.js'],
+                dest: 'build/js/build.js'
             }
         },
- 
-        uglify: { 
+
+        uglify: {
             options: {
                 stripBanners: true,
-                banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n' 
+                banner: '/* <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %> */\n'
             },
- 
+
             build: {
-                src: 'build/build.js', 
-                dest: 'build/build.min.js'
+                src: 'build/js/build.js',
+                dest: 'build/js/build.min.js'
             }
         },
- 
-        cssmin: { 
+
+        cssmin: {
             with_banner: {
                 options: {
                     banner: '/* Choose a cat min styles */'
                 },
- 
+
                 files: {
-                    'build/style.min.css' : ['app/css/*.css']
+                    'build/css/style.min.css' : ['app/css/*.css']
                 }
             }
         },
- 
-        watch: { 
+
+        watch: {
             scripts: {
-                files: ['app/js/*.js'], 
-                tasks: ['jshint', 'concat', 'uglify', 'removelogging'] 
+                files: ['app/js/*.js'],
+                tasks: ['jshint', 'concat', 'uglify', 'removelogging']
             },
             css: {
-                files: ['app/css/*.css'], 
-                tasks: ['cssmin'] 
+                files: ['app/css/*.css'],
+                tasks: ['cssmin']
             }
         },
- 
- 
-        removelogging: { 
+
+        removelogging: {
             dist: {
-                src: "build/build.min.js", 
-                dest: "build/build.min.js" 
+                src: "build/js/build.min.js",
+                dest: "build/js/build.min.js"
             }
         },
 
@@ -81,28 +80,40 @@ module.exports = function (grunt) {
                 desc: "build/img/"
             }
         },
-        
+
         'gh-pages': {
             options: {
-            base: 'app'
+            base: 'build'
             },
             src: ['**']
+        },
+        copy: {
+            main: {
+                files: [
+                    {expand: true, cwd: 'app/', src: ['cars/*'], dest: 'build/', filter: 'isFile'},
+                    {expand: true, cwd: 'app/', src: ['img/**/*'], dest: 'build'},
+                    {expand: true, cwd: 'app/', src: ['partials/*'], dest: 'build/', filter: 'isFile'},
+                    {expand: true, cwd: 'app/', src: ['*.html'], dest: 'build'}
+                ]
+            }
         }
- 
- 
+
     });
- 
-    //подгружаем необходимые плагины
+
+    //use grants plugin for tasks
     // grunt.loadNpmTasks('grunt-contrib-jshint');
-    // grunt.loadNpmTasks('grunt-contrib-concat');
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-cssmin');
     // grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.loadNpmTasks('grunt-remove-logging');
-    // grunt.loadNpmTasks('grunt-imgo');
+    // grunt.loadNpmTasks('grunt-imgo'); //not work on windows
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-remove-logging');
+    grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-gh-pages');
- 
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'removelogging']); 
-    grunt.registerTask('build', ['jshint', 'imgo', 'concat', 'uglify', 'cssmin', 'removelogging', 'watch']); 
-    grunt.registerTask('pages', ['gh-pages']);  
+
+    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'removelogging']);
+
+    //preparing for publish on gh-pages
+    grunt.registerTask('build', [ 'concat', 'removelogging','uglify', 'cssmin', 'copy']);
+    grunt.registerTask('pages', ['gh-pages']);
 };
